b41944672833a6b29b6845a0339e8e5acc411023
zarnas <zarnas@zignuts.com>
2022-02-28 16:32:48 +0530

    done

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+const mysql = require('mysql')
 const app = express();
-require("./src/db/conn")
-const Blog = require("./src/models/blog");
+
 const path = require("path");
 
 const router = require("./router/routes");
-app.use('/',router);
+app.use('/', router);
 
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json());
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.post('/add',async(req,res)=>{
-    console.log(req.body)
-    try{
-        const blog = new Blog(req.body)
-        const createblog = await blog.save();
-        res.status(201).send(createblog);
-    }catch(e){res.status(400).send(e);}
-})
-
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log('http://localhost:8000');
 });
--- router/routes.js
+++ router/routes.js
@@ -1,28 +1,86 @@
 const express = require("express");
+const { route } = require("express/lib/application");
+var bodyParser = require("body-parser");
 const router = express.Router();
+const mysql = require('mysql');
+const { response } = require("express");
 
-router.get("/",(req,res)=>{
-    res.render("index2");
-})
-router.get("/new",(req,res)=>{
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }))
+
+router.get("/new", (req, res) => {
     res.render('new')
 })
 
-router.get("/category",(req,res)=>{
-    res.render('category')
-})
+// connecton start
+const connection = mysql.createConnection({
+    host: '15.206.7.200',
+    port: "3310",
+    user: 'zarnas',
+    password: '9XB7bCWfgJqKyjRtGZ4vvfM68RunVWc5',
+    database: 'zarnas'
 
-router.get("/post",(req,res)=>{
-    res.render('post')
 })
-router.post('/add',async(req,res)=>{
-    console.log(req.body)
-    try{
-        const blog = new Blog(req.body)
-        const createBlog = await blog.save();
-        res.status(201).send(createBlog);
-    }catch(e){res.status(400).send(e);}
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
 })
+// connection end
+
+router.get('/', function (req, res) {
+    let sql = "SELECT * FROM blogs";
+    connection.query(sql, function (error, result) {
+        if (error) { throw error; }
+        res.render("index2", { data: result });
+    });
+    console.log("displayed on 8000")
+});
+
+router.get("/all/:id", (req, res) => {
+    let bid = req.params.id;
+    let sql = `SELECT * FROM blogs WHERE id=${bid}`;
+    connection.query(sql, function (error, result) {
+        if (error) { throw error; }
+        res.render("show", { data: result });
+    });
+});
+    router.get("/cat/s", (req, res) => {
+        let sql = "SELECT * FROM blogs WHERE c_id='sports'";
+        connection.query(sql, function (error, result) {
+            if (error) { throw error; }
+            res.render("show", { data: result });
+        });
+
+    });
+
+    router.get("/cat/m", (req, res) => {
+        let sql = "SELECT * FROM blogs WHERE c_id='music'";
+        connection.query(sql, function (error, result) {
+            if (error) { throw error; }
+            res.render("show", { data: result });
+        });
+
+    });
+    router.get("/cat/:id", (req, res) => {
+        let bid = req.params.id;
+        let sql = `SELECT * FROM blogs WHERE id=${bid}`;
+        connection.query(sql, function (error, result) {
+            if (error) { throw error; }
+            res.render("show", { data: result });
+        });
+    });
+
+    router.post("/add", function (req, res) {
+        var sql = "INSERT INTO blogs (title,content,c_id) VALUES ?";
+        var values = [[req.body.title, req.body.content,req.body.category]]
+        console.log(req.body.category);
+        connection.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("record inserted" + result.affectedRows)
+
+        });
+    });
+
 
 
 
