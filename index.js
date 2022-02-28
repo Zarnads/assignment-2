0fe5e77e83f3ab709dd86fd6b501749e0b53477b
zarnas <zarnas@zignuts.com>
2022-02-28 18:52:42 +0530

    done.

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const mysql = require('mysql')
 const app = express();
 
 const path = require("path");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 const router = require("./router/routes");
 app.use('/', router);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json());
-
-app.set("view engine", "ejs");
-
 app.listen(8000, () => {
     console.log('http://localhost:8000');
 });
+
+
+
+
+
